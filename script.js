f498edb3c6c3d552324cc02c631b53568f6bf273
Alex Robert <contact.alexrobert04@gmail.com>
2021-08-15 17:16:04 +0200

    Add error style for bill field

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+const billInput = document.getElementsByName('bill')[0]
+const billError = document.querySelector('#bill > span.textError')
+
 const fieldVerification = (input) => {
   return parseInt(input)
 }
+
+billInput.addEventListener('input', (event) => {
+  const text = event.target.value
+  if (text > 100000) {
+    billInput.classList.add('error')
+    billError.style.display = 'block'
+  } else {
+    billInput.classList.remove('error')
+    billError.style.display = 'none'
+  }
+})
 
 module.exports = {
   fieldVerification
